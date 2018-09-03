b78077e26d603a0a9d6dd63e455c511ac446f457
rachelung <42155462+rachelung@users.noreply.github.com>
2018-09-03 14:56:23 -0700

    Update and rename Question.py to question.py

--- question.py
+++ question.py
@@ -9,39 +9,39 @@ def index():
 def render_q1():
     return render_template('q1.html')
 
-@app.route('q2')
+@app.route('/q2')
 def render_q2():
     return render_template('q2.html')
 
-@app.route('q3')
+@app.route('/q3')
 def render_q3():
     return render_template('q3.html')
 
-@app.route('q4')
+@app.route('/q4')
 def render_q4():
     return render_template('q4.html')
 
-@app.route('q5')
+@app.route('/q5')
 def render_q5():
     return render_template('q5.html')
 
-@app.route('q6')
+@app.route('/q6')
 def render_q6():
     return render_template('q6.html')
 
-@app.route('q7')
+@app.route('/q7')
 def render_q7():
     return render_template('q7.html')
 
-@app.route('q8')
+@app.route('/q8')
 def render_q8():
     return render_template('q8.html')
 
-@app.route('q9')
+@app.route('/q9')
 def render_q9():
     return render_template('q9.html')
 
-@app.route('q10')
+@app.route('/q10')
 def render_q10():
     return render_template('q10.html')
 
